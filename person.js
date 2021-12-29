43ec1270708ce7b22a4050713dfe5369d9e5f7b7
amreeth <amreethashok@gmail.com>
2021-12-29 15:13:46 +0530

    first-commit

---
+++ person.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#submit-form").validate({
+        rules:{
+            name:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            number:{
+                required:true,
+                minlength:10,
+                maxlength:10
+            },
+            message:{
+                required:true,
+                minlength:5,
+                maxlength:400
+            }
+        }
+    })
+})
